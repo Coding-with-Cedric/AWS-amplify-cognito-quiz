9d8975a72e35c45efb1163523559e34ac35b560b
Coding-with-Cedric <142952939+Coding-with-Cedric@users.noreply.github.com>
2024-11-06 20:35:09 -0600

    Update quizData.js

--- src/quizData.js
+++ src/quizData.js
@@ -1,21 +1,21 @@
 const quizData = [
     {
-      question: "In the *Harry Potter* series, what is the name of Harry's pet owl?",
+      question: "In the Harry Potter series, what is the name of Harry's pet owl?",
       options: ["Hedwig", "Errol", "Hermes", "Bubo"],
       answer: "Hedwig"
     },
     {
-      question: "In the movie *Star Wars*, who said *I am your father*?",
+      question: "In the movie Star Wars, who said I am your father?",
       options: ["Yoda", "Obi-Wan Kenobi", "Darth Vader", "Captain Kirk"],
       answer: "Darth Vader"
     },
     {
-      question: "What year was the first *Toy Story* movie released?",
+      question: "What year was the first Toy Story movie released?",
       options: ["1996", "1995", "1997", "1998"],
       answer: "1995"
     },
     {
-      question: "Which film is known for the famous quote, *I'll be back*?",
+      question: "Which film is known for the famous quote, I'll be back?",
       options: ["Shrek", "Jumanji", "The Matrix", " The Terminator"],
       answer: "The Terminator"
     },
@@ -25,17 +25,17 @@ const quizData = [
       answer: "Philadelphia"
     },
     {
-      question: "*In the Wizard of Oz*, what does the Scarecrow want from the wizard?",
+      question: "In the Wizard of Oz, what does the Scarecrow want from the wizard?",
       options: ["A brain", "A heart", "Courage", "A better wardrobe"],  
       answer: "A brain"
     },
     {
-      question: "Who directed *Jaws*?",
+      question: "Who directed Jaws?",
       options: ["Jaws Himself", "Christopher Nolan", "Steven Spielberg", "Alfred Hitchcock"],
       answer: "Steven Spielberg"
     },
     {
-      question: "*In Back to the Future*, how fast does the Delorean need to go to time travel?",
+      question: "In Back to the Future, how fast does the Delorean need to go to time travel?",
       options: ["55 mph", "88 mph", "99 mph", "100 mph"],
       answer: "88 mph"
     },
@@ -45,22 +45,22 @@ const quizData = [
       answer: "Ghostbusters"
     },
     {
-      question: "In *The Hunger Games*, what district is Katniss Everdeen from?",
+      question: "In The Hunger Games, what district is Katniss Everdeen from?",
       options: ["District 7", "District 12", "District 3", "District 100"],
       answer: "District 12"     
     },  
     {
-      question: "In *The Matrix*, what color pill does Neo take?",
+      question: "In The Matrix, what color pill does Neo take?",
       options: ["Purplek", "Blue", "Green", "Red"],
       answer: "Red"
     },
     {
-      question: "In *Avengers: Endgame*, which character says, *I love you 3000*?",
+      question: "In Avengers: Endgame, which character says, I love you 3000?",
       options: ["Tony Stark (Iron Man)", "Peter Parker (Spiderman)", "Morgan Stark", "Steve Rogers (Captain America)"],  
       answer: "Morgan Stark"
     },
     {
-      question: "In *Interstellar*, what is the name of the fictional NASA pilot who is sent on a mission to find a new home for humanity?",  
+      question: "In Interstellar, what is the name of the fictional NASA pilot who is sent on a mission to find a new home for humanity?",  
       options: ["Cooper", "Miller", "Brand", "Doyle"],
       answer: "Cooper"
     }
