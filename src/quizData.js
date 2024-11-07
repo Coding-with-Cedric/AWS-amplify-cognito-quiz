a2839f9fd362235f075c44d3ecf166e60bbaa9ee
Coding-with-Cedric <142952939+Coding-with-Cedric@users.noreply.github.com>
2024-11-06 20:27:48 -0600

    Update quizData.js

--- src/quizData.js
+++ src/quizData.js
@@ -1,68 +1,68 @@
 const quizData = [
     {
-      question: "In the Harry Potter series, what is the name of Harry's pet owl?",
+      question: "In the *Harry Potter* series, what is the name of Harry's pet owl?",
       options: ["Hedwig", "Errol", "Hermes", "Bubo"],
       answer: "Hedwig"
     },
     {
-      question: "In the movie Star Wars, who said I am your father?",
+      question: "In the movie *Star Wars*, who said *I am your father*?",
       options: ["Yoda", "Obi-Wan Kenobi", "Darth Vader", "Captain Kirk"],
       answer: "Darth Vader"
     },
     {
-      question: "What year was the first Toy Story movie released?",
+      question: "What year was the first *Toy Story* movie released?",
       options: ["1996", "1995", "1997", "1998"],
       answer: "1995"
     },
     {
-      question: "Which film is known for the famous quote, I'll be back?",
+      question: "Which film is known for the famous quote, *I'll be back*?",
       options: ["Shrek", "Jumanji", "The Matrix", " The Terminator"],
       answer: "The Terminator"
     },
     {
-      question: "Which programming language is known as the backbone of the World Wide Web?",
-      options: ["Java", "C#", "Python", "HTML"],
-      answer: "HTML"
+      question: "What movie did Tom Hanks win his first Academy Award for?",
+      options: ["Philadelphia", "Cast Away", "Saving Private Ryan", "Forrest Gump"],
+      answer: "Philadelphia"
     },
     {
-      question: "What is the name of the world's first computer programmer?",
-      options: ["Charles Babbage", "Ada Lovelace", "Alan Turing", "Grace Hopper"],
-      answer: "Ada Lovelace"
+      question: "*In the Wizard of Oz*, what does the Scarecrow want from the wizard?",
+      options: ["A brain", "A heart", "Courage", "A better wardrobe"],  
+      answer: "A brain"
     },
     {
-      question: "In what year was the iPhone first introduced?",
-      options: ["2005", "2007", "2009", "2011"],
-      answer: "2007"
+      question: "Who directed *Jaws*?",
+      options: ["Jaws Himself", "Christopher Nolan", "Steven Spielberg", "Alfred Hitchcock"],
+      answer: "Steven Spielberg"
     },
     {
-      question: "What was Google's original name?",
-      options: ["BackRub", "Googol", "SearchMaster", "WebSearch"],
-      answer: "BackRub"
+      question: "*In Back to the Future*, how fast does the Delorean need to go to time travel?",
+      options: ["55 mph", "88 mph", "99 mph", "100 mph"],
+      answer: "88 mph"
     },
     {
-      question: "Which of these companies was not founded in a garage?",
-      options: ["Amazon", "Google", "Apple", "Microsoft"],
-      answer: "Amazon"
+      question: "Who are you going to call?",
+      options: ["The Police", "Paranormal Investigators", "Ghostbusters", "Drake's Hotline Bling"],
+      answer: "Ghostbusters"
     },
     {
-      question: "What does 'GPU' stand for?",
-      options: ["Graphical Processing Unit", "Graphics Performance Unit", "Graphics Processing Unit", "Graphical Performance Unit"],
-      answer: "Graphics Processing Unit"
+      question: "In *The Hunger Games*, what district is Katniss Everdeen from?",
+      options: ["District 7", "District 12", "District 3", "District 100"],
+      answer: "District 12"     
     },  
     {
-      question: "What is the capital of France?",
-      options: ["New York", "London", "Paris", "Dublin"],
-      answer: "Paris"
+      question: "In *The Matrix*, what color pill does Neo take?",
+      options: ["Purplek", "Blue", "Green", "Red"],
+      answer: "Red"
     },
     {
-      question: "Who painted the Mona Lisa?",
-      options: ["Vincent Van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet"],
-      answer: "Leonardo da Vinci"
+      question: "In *Avengers: Endgame*, which character says, *I love you 3000*?",
+      options: ["Tony Stark (Iron Man)", "Peter Parker (Spiderman)", "Morgan Stark", "Steve Rogers (Captain America)"],  
+      answer: "Morgan Stark"
     },
     {
-      question: "What is the largest planet in our solar system?",
-      options: ["Earth", "Jupiter", "Saturn", "Mars"],
-      answer: "Jupiter"
+      question: "In *Interstellar*, what is the name of the fictional NASA pilot who is sent on a mission to find a new home for humanity?",  
+      options: ["Cooper", "Miller", "Brand", "Doyle"],
+      answer: "Cooper"
     }
   ];
   
